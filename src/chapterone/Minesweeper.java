301dac4de5c16983594368022c6be1fd883d2066
Aaron <hanrahanaaron@gmail.com>
2019-11-21 21:24:43 -0600

    Did Main Method

--- src/chapterone/Minesweeper.java
+++ src/chapterone/Minesweeper.java
@@ -1,11 +1,73 @@
 package chapterone;
 
-// Minesweeper problem
+/*
+ * Sample input
+ * 
+ * 4 4
+   *...
+   ....
+   .*..
+   ....
+   
+   3 5
+   **...
+   .....
+   .*...
+   
+   0 0
+ */
+
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
 public class Minesweeper {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in;
+		
+		in = new Scanner(new BufferedInputStream(System.in));
+		
+		while(in.hasNext()) {
+			int fieldNo = 0;
+			int n = in.nextInt();
+			int m = in.nextInt();
+			
+			// If the numbers are zero then end the input, i.e. break.
+			if (n == 0 || m == 0) {
+				break;
+			}
+			
+			char[][] field = new char[n][m];
+			
+			for (int i = 0; i < n; i++) {
+				String line = in.next();
+				
+				for (int j = 0; j < m; j++) {
+					field[i][j] = line.charAt(j);
+				}
+			}
+			
+			fieldNo++;
+			printNewField(n, m, field, fieldNo);
+		}
+		
+		in.close();
+	}
+	
+	static void printNewField(int n, int m, char[][] field, int fieldNo) {
+		// Adds a space between input and other fields.
+		System.out.println();
+		
+		// Currently in here for testing, prints out the same exact field as before.
+		for (int i = 0; i < n; i++) {
+			String newLine = "";
+			
+			for (int j = 0; j < m; j++) {
+				newLine += field[i][j];
+			}
+			
+			System.out.println(newLine);
+		}
 	}
 
 }
